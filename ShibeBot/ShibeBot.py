41a2290b9883c803f68963a8db14090a9611e59b
TheRealSHibe <andrewdkennedy1@gmail.com>
2017-02-01 22:10:33 -0800

    try embed

--- ShibeBot/ShibeBot.py
+++ ShibeBot/ShibeBot.py
@@ -16,10 +16,11 @@ class Shibe:
 
     @commands.command()
     async def ark(self):
-        await self.bot.say("Click To Connect to Mandrew ARK")
-        await self.bot.say("steam://connect/71.93.28.250:27015")
-        await self.bot.say("Whisper a Doge Master for the password")
-        await self.bot.say("NOTE: If it's your first time, You'll have to reconnect multiple times until all the mods are loaded. You can track workshop content download progress in your steam client inbetween reconnects")
+        data = "Click To Connect to Mandrew ARK"
+"steam://connect/71.93.28.250:27015"
+"Whisper a Doge Master for the password"
+"NOTE: If it's your first time, You'll have to reconnect multiple times until all the mods are loaded. You can track workshop content download progress in your steam client inbetween reconnects"
+        await self.bot.say(embed=data)
 
 def setup(bot):
     n = Shibe(bot)
