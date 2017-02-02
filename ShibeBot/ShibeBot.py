881790446ee853d079c460be27dadb3b31678ab9
therealshibe <andrewdkennedy1@gmail.com>
2017-02-01 16:26:27 -0800

    suh
    suh

--- ShibeBot/ShibeBot.py
+++ ShibeBot/ShibeBot.py
@@ -12,7 +12,7 @@ class Shibe:
 
     @commands.command()
     async def suh(self):
-        await self.bot.say(" :snake:  SUH DUDE :snake: ")
+        await self.bot.say(" :suhdude: SUH DUDE :suhdude: ")
 
 def setup(bot):
     n = Shibe(bot)
