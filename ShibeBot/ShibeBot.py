c87c33d9b5bf252d87b316d2b7015922f178543c
TheRealSHibe <andrewdkennedy1@gmail.com>
2017-02-01 22:40:53 -0800

    a
    a

--- ShibeBot/ShibeBot.py
+++ ShibeBot/ShibeBot.py
@@ -21,7 +21,7 @@ class Shibe:
     @commands.command()
     async def ark(self):
         em = discord.Embed(title='My Embed Title', description='My Embed Content.', colour=0xDEADBF)
-        em.set_author(name='Someone', icon_url=http://i.imgur.com/mYdLixs.png)
+        em.set_author(name='Mandrew ARK', icon_url='http://i.imgur.com/mYdLixs.png')
         await client.send_message(message.channel, embed=em)
 
 
