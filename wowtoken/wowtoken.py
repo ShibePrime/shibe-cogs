d8ead4d56a44abca84284aa309001994119f7492
therealshibe <andrewdkennedy1@gmail.com>
2017-02-14 11:24:56 -0800

    wowtoken

--- wowtoken/wowtoken.py
+++ wowtoken/wowtoken.py
@@ -70,7 +70,7 @@ class WoWTokenInfo:
 
 def setup(bot):
     if soup_available and pypandoc_available:
-        bot.add_cog(BlizzPatchNotes(bot))
+        bot.add_cog(wowtokeninfo(bot))
     else:
         if not soup_available:
             error_text += "You need to run `pip install beautifulsoup4`\n"
