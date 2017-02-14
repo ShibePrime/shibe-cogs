4721f971517a22e0ca1751b2115933e2458f4bd6
therealshibe <andrewdkennedy1@gmail.com>
2017-02-14 11:08:44 -0800

    wowtoken

---
+++ wowtoken/wowtoken.py
@@ -0,0 +1,79 @@
+import discord
+from discord.ext import commands
+from .utils.chat_formatting import pagify
+import aiohttp
+
+try:  # check if BeautifulSoup4 is installed
+    from bs4 import BeautifulSoup
+    soup_available = True
+except:
+    soup_available = False
+
+try:  # check if pypandoc is installed
+    import pypandoc
+    pypandoc_available = True
+except:
+    pypandoc_available = False
+
+# Special thanks to judge2020 for telling me about pandoc, and offering their
+# code as a reference: https://github.com/judge2020/BattleNetUpdateChecker
+
+# This cog requires:
+# BeautifulSoup4 :: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+# pypandoc :: https://pypi.python.org/pypi/pypandoc
+# Pandoc :: http://pandoc.org/
+
+base_url = 'https://wowtoken.info/'
+# product_url = '/patch-notes?productType='
+# hearthstone_abbr = 'wtcg'
+# overwatch_abbr = 'Pro'
+# starcraft2_abbr = 'sc2'
+# warcraft_abbr = 'WoW'
+# diablo_abbr = 'd3'
+# hots_abbr = 'heroes'
+# headers = {'User-Agent': 'Battle.net/1.0.8.4217'}
+
+
+class WoWTokenInfo:
+
+    """Blizzard Game Patch Notes"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="wowtoken", pass_context=True)
+    async def wowtoken(self, ctx):
+        """Prints latest WoW Token Price NA"""
+
+        url = ''.join([base_url])
+
+        await self.print_patch_notes(url)
+
+    async def print_patch_notes(self, url):
+        try:
+            async with aiohttp.get(url, headers=headers) as response:
+                soup = BeautifulSoup(await response.text(), "html.parser")
+
+            html_notes = soup.find('div', {"class": "mui-panel realm-panel"}, {"id": "na-panel"})
+            text_notes = pypandoc.convert_text(html_notes, 'plain',
+                                               format='html',
+                                               extra_args=['--wrap=none'])
+            text_notes = text_notes.replace('&nbsp;', ' ')
+            text_notes = text_notes.replace('&apos;', "'")
+            msg_list = pagify(text_notes, delims=["\n"])
+            for msg in msg_list:
+                await self.bot.say(msg)
+
+        except:
+            await self.bot.say("I suck at python.")
+
+
+def setup(bot):
+    if soup_available and pypandoc_available:
+        bot.add_cog(BlizzPatchNotes(bot))
+    else:
+        if not soup_available:
+            error_text += "You need to run `pip install beautifulsoup4`\n"
+        if not pypandoc_available:
+            error_text += "You need to run `pip install pypandoc`\n"
+        raise RuntimeError(error_text)
