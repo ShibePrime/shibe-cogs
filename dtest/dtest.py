4a13409d0d19091313637ce7ff8fb6b3b82f06a8
TheRealSHibe <andrewdkennedy1@gmail.com>
2017-11-16 19:40:40 -0800

    1
    2

--- dtest/dtest.py
+++ dtest/dtest.py
@@ -44,7 +44,8 @@ class ddb:
     async def print_notes(self, url):
         try:
             async with aiohttp.post(url, json=payload, headers=headers) as response:
-			print (response.text())
+			results = await response.text()
+			print(results)
             soup = BeautifulSoup(await response.text(), "html.parser")
             html_notes = soup.find('div', {"class": "home-page"})
             text_notes = pypandoc.convert_text(html_notes, 'plain',
