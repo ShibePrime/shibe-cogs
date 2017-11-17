46334fc3e6cb60f108ebaa04895adb2d9af1d86d
TheRealSHibe <andrewdkennedy1@gmail.com>
2017-11-17 10:55:46 -0800

    y break?
    quotes

--- dtest/dtest.py
+++ dtest/dtest.py
@@ -78,7 +78,7 @@ class ddb:
                 print(nightfallInfo)
                 print(imageurl)
                 em = discord.Embed(title='', description=nightfallInfo, colour=0xFFD966)
-                em.set_author(name='Destiny Weekly Info', icon_url='https://cdn.thetrackernetwork.com/destiny//common/destiny2_content/icons/4de13648c3bf8741e773d8e7e8a0164b.png',url="http://db.destinytracker.com/d2/en")
+                em.set_author(name='Destiny Weekly Info', icon_url="https://cdn.thetrackernetwork.com/destiny//common/destiny2_content/icons/4de13648c3bf8741e773d8e7e8a0164b.png",url="http://db.destinytracker.com/d2/en")
                 await self.bot.say(embed=em)
 
 
