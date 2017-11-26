6b0efb109f5df3e150629d5c609bf84218db3fc9
TheRealSHibe <andrewdkennedy1@gmail.com>
2017-11-26 00:23:02 -0800

    rip xkcd doge cog
    removed from mine cuz someone else

--- doge/doge.py
+++ doge/doge.py
@@ -176,22 +176,6 @@ class Shibe:
         pecker = random.choice(lines)
         await self.bot.say(pecker)
 
-    # --xkcd by ultimatePancake
-    @commands.group(name="xkcd", pass_context=True)
-    async def xkcd(self, ctx):
-        """Displays latest xkcd comic"""
-        if ctx.invoked_subcommand is None:
-            await self.bot.say(xkcd.getLatestComic().getImageLink())
-
-    @xkcd.command(name="random", pass_context=True)
-    async def _random(self, ctx):
-        """Displays random xkcd comic."""
-        await self.bot.say(xkcd.getRandomComic().getImageLink())
-
-    @xkcd.command(name="number", pass_context=True)
-    async def _number(self, ctx, number: int):
-        """Displays specified xkcd comic."""
-        await self.bot.say(xkcd.getComic(number).getImageLink())
 
 
 # --
