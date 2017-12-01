9f04021a1a7c47d1289542d1459aea84576899a8
therealshibe <andrewdkennedy1@gmail.com>
2017-11-30 22:26:38 -0800

    Word2Num
    Word2Num

--- spellnum/spellnum.py
+++ spellnum/spellnum.py
@@ -1,6 +1,6 @@
 from num2words import num2words
 from discord.ext import commands
-
+from word2number import w2n
 
 class spellnum:
     """Shows the word of a number"""
@@ -13,6 +13,12 @@ class spellnum:
         """spellnum"""
         i = int(num)
         wordnum = num2words(i)
+        await self.bot.say(wordnum)
+
+    @commands.command(pass_context=True)
+    async def wordnum(self, context, word):
+        """wordnum"""
+        wordnum = w2n.word_to_num(word)
         await self.bot.say(wordnum)
 
 
