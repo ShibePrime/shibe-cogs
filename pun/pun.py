571bbbcf7ad2773b593db25afe29434182d129ce
therealshibe <andrewdkennedy1@gmail.com>
2018-01-13 00:59:17 -0800

    Format
    lol

--- pun/pun.py
+++ pun/pun.py
@@ -2,13 +2,11 @@ import discord
 from discord.ext import commands
 import aiohttp
 
-
 base_url = 'https://pun.andrewmacheret.com/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
 
 
 class pun:
-
     """pun"""
 
     def __init__(self, bot):
@@ -20,18 +18,22 @@ class pun:
 
         url = ''.join([base_url])
 
-        await self.print_pun(url)
+        await
+        self.print_pun(url)
 
     async def print_pun(self, url):
         try:
             async with aiohttp.get(url, headers=headers) as response:
-            pun = await response.text()
+                pun = await
+            response.text()
             em = discord.Embed(description=pun, colour=0xFFD966)
-            await self.bot.say(embed=em)
+            await
+            self.bot.say(embed=em)
 
         except:
-            await self.bot.say("Error")
+            await
+            self.bot.say("Error")
 
 
 def setup(bot):
-        bot.add_cog(pun(bot))
+    bot.add_cog(pun(bot))
