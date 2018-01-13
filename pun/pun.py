0b13259fd6a9149f557bfd843068177a90e1a03c
therealshibe <andrewdkennedy1@gmail.com>
2018-01-13 01:15:43 -0800

    pre json
    full dump

--- pun/pun.py
+++ pun/pun.py
@@ -22,9 +22,8 @@ class pun:
         async with ClientSession() as session:
             async with session.post(url, headers=headers) as response:
                 data = await response.json()
-                pun = data[0]
                 imageurl = 'https://i.pinimg.com/originals/28/c1/1c/28c11c84de44f79450c8794849d02ab5.jpg'
-                pun = "__**" + pun[0] + "**__"
+                pun = "__**" + data + "**__"
                 em = discord.Embed(title='', description=pun, colour=0xFFD966, )
                 em.set_thumbnail(url=imageurl)
                 em.set_author(name='ShibeBot The Pun Lord')
