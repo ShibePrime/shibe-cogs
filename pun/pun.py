c28b53da1ce515123fc62e4d5b7d86d295120b65
therealshibe <andrewdkennedy1@gmail.com>
2018-01-13 01:10:09 -0800

    json
    who dis?

--- pun/pun.py
+++ pun/pun.py
@@ -1,39 +1,34 @@
+import json
 import discord
+from aiohttp import ClientSession
 from discord.ext import commands
-import aiohttp
 
-base_url = 'https://pun.andrewmacheret.com/'
+url = 'https://pun.andrewmacheret.com/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
 
 
 class pun:
-    """pun"""
+    """Random Puns"""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="wowtoken", pass_context=True)
+    @commands.command(name="pun", pass_context=True)
     async def pun(self, ctx):
-        """Prints random pun """
+        await self.get_pun()
+        """Prints Random Puns"""
 
-        url = ''.join([base_url])
-
-        await
-        self.print_pun(url)
-
-    async def print_pun(self, url):
-        try:
-            async with aiohttp.get(url, headers=headers) as response:
-                pun = await
-            response.text()
-            em = discord.Embed(description=pun, colour=0xFFD966)
-            await
-            self.bot.say(embed=em)
-
-        except:
-            await
-            self.bot.say("Error")
-
+    async def get_pun(self):
+        async with ClientSession() as session:
+            async with session.post(url, headers=headers) as response:
+                data = await response.json()
+                pun = data["pun"]
+                imageurl = 'https://i.pinimg.com/originals/28/c1/1c/28c11c84de44f79450c8794849d02ab5.jpg'
+                pun = "__**" + pun[0] + "**__"
+                em = discord.Embed(title='', description=pun, colour=0xFFD966, )
+                em.set_thumbnail(url=imageurl)
+                em.set_author(name='ShibeBot The Pun Lord')
+                await self.bot.say(embed=em)
 
 def setup(bot):
     bot.add_cog(pun(bot))
