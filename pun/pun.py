fc9c8d6acec0550320b44b0fb41bd9d36b86d4fa
therealshibe <andrewdkennedy1@gmail.com>
2018-01-13 01:33:11 -0800

    asdf
    asdf

--- pun/pun.py
+++ pun/pun.py
@@ -22,13 +22,14 @@ class pun:
         async with ClientSession() as session:
             async with session.get(url, headers=headers) as response:
                 onepun = await response.json()
-                print onepun
                 imageurl = 'https://i.pinimg.com/originals/28/c1/1c/28c11c84de44f79450c8794849d02ab5.jpg'
                 pun = "__**" + onepun + "**__"
                 em = discord.Embed(title='', description=pun, colour=0xFFD966, )
                 em.set_thumbnail(url=imageurl)
                 em.set_author(name='ShibeBot The Pun Lord')
                 await self.bot.say(embed=em)
+
+print onepun
 
 def setup(bot):
     bot.add_cog(pun(bot))
