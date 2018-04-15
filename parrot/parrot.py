801085604c126c53b9e90d0bdd09789d4c622abf
therealshibe <andrewdkennedy1@gmail.com>
2018-04-14 20:39:02 -0700

    Themed for Raid TOol
    Gettinging ready for PR to raidcog

--- parrot/parrot.py
+++ parrot/parrot.py
@@ -9,52 +9,43 @@ from datetime import datetime
 path = 'data/parrot'
 
 class parrot:
-    """Waterfall Convo Repeat and Join"""
+    """Waterfall Convo Parrot"""
 
     def __init__(self, bot):
         self.bot = bot
 
 
     @commands.command(name="parrot", pass_context=True)
-    async def parrot(self, ctx, name: str):
+    async def parrot(self, ctx, game: str):
         """Interactive prompt for making a raid"""
         author = ctx.message.author
         server = ctx.message.server
 
         await self.bot.say("I will message you to continue.")
-        await self.contact_for_parrot(name, author, server)
+        await self.contact_for_parrot(game, author, server)
 
-    async def contact_for_parrot(self, name: str, author, server=None):
+    async def contact_for_parrot(self, game: str, author, server=None):
 
         dm = await self.bot.send_message(author,
                                          "Please respond to this message "
-                                         "with the title of your embed. If "
-                                         "you do not want a title, wait 30s")
-        title = await self.bot.wait_for_message(channel=dm.channel,
+                                         "with the details of your raid. If "
+                                         "you do not want a details, wait 30s"
+                                         "Ex: Prestige Argos ; WoW Mythics ; LFR")
+        desc = await self.bot.wait_for_message(channel=dm.channel,
                                                 author=author, timeout=30)
 
-        if title is None:
+        if desc is None:
             await self.bot.send_message(author,
-                                        "Okay, this one won't have a title.")
+                                        "Okay, this one won't have a description.")
 
         dm = await self.bot.send_message(author,
-                                         "Please respond to this message "
-                                         "with the content of your embed")
-        message = await self.bot.wait_for_message(channel=dm.channel,
+                                         "what time? Follow the example format. MM/DD/YY Hr:MinAM/PM TIMEZONE "
+                                         "Ex: 12/25/17 8:00pm PST")
+        time = await self.bot.wait_for_message(channel=dm.channel,
                                                   author=author, timeout=120)
 
-        if message is None:
-            if server is not None:
-                self.settings[server.id]['usercache'].remove(author.id)
-            else:
-                self.settings['global']['usercache'].remove(author.id)
-            self.save_settings()
-            return await self.bot.send_message(author,
-                                               "I can't wait forever, "
-                                               "try again when ready")
-        else:
-            await self.bot.say(name, title, message, server)
-            await self.bot.send_message(author, "Your raid was created")
+        await self.bot.say(game, desc, time, server)
+        await self.bot.send_message(author, "Your raid was created")
 
 def setup(bot):
     bot.add_cog(parrot(bot))
