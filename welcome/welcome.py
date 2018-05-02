58e43eeb46a04196ba594a8cb7462fb615ad01be
therealshibe <andrewdkennedy1@gmail.com>
2018-05-01 21:58:51 -0700

    gloab
    get

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -7,13 +7,13 @@ class welcome:
     """welcome a new DOGE Using The Bot with hardcoded welcome message"""
     def __init__(self, bot):
         self.bot = bot
-        msg = msg='*Hello, this is doge!* :wolf:\r\nI\'m here to inform you of the new role you\'ve been granted by the mighty doges of the Thunderdoge.\r\nMuch congrats!\r\nNow that you\'re officially part of the pack you can use bot commands in `#botplayground` to assign yourself roles and join game specific text channels.\r\nVisit the `#announcements` text channel under the \"General Text Chat\" category to view these roles\/commands.\r\nThank you for joining us!\r\nIf you need help with anything, please bork in the `#halp_pls` channel or bork at a DogeMaster\/DogeMod\r\nWelcome to the Thunderdoge!'
+        msg = '*Hello, this is doge!* :wolf:\r\nI\'m here to inform you of the new role you\'ve been granted by the mighty doges of the Thunderdoge.\r\nMuch congrats!\r\nNow that you\'re officially part of the pack you can use bot commands in `#botplayground` to assign yourself roles and join game specific text channels.\r\nVisit the `#announcements` text channel under the \"General Text Chat\" category to view these roles\/commands.\r\nThank you for joining us!\r\nIf you need help with anything, please bork in the `#halp_pls` channel or bork at a DogeMaster\/DogeMod\r\nWelcome to the Thunderdoge!'
 
     @commands.command(pass_context=True)
     @checks.is_owner()
     async def welcome(self, ctx, user_id: str):
         """welcome a new DOGE using ShibBot with hardcoded welcome message in a DM"""
-        username = message.server.members.get("name", user_id).id;
+        username = client.users.get("name", user_id).id;
         user = await self.bot.get_user_info(username)
         try:
             e = discord.Embed(colour=discord.Colour.red())
