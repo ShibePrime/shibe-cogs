98a6424a36777fa4639cae6c273f78fc0d620d6c
therealshibe <andrewdkennedy1@gmail.com>
2018-05-01 23:45:11 -0700

    frmt
    asdf

--- parrot/parrot.py
+++ parrot/parrot.py
@@ -8,13 +8,12 @@ from datetime import datetime
 
 path = 'data/parrot'
 
+
 class parrot:
     """Waterfall Convo Parrot"""
 
     def __init__(self, bot):
         self.bot = bot
-
-
 
     @commands.command(name="parrot", pass_context=True)
     async def parrot(self, ctx, game: str):
@@ -22,35 +21,48 @@ class parrot:
         author = ctx.message.author
         server = ctx.message.server
 
-        await self.bot.say("I will message you to continue.")
-        await self.contact_for_parrot(game, author, server)
+        await
+        self.bot.say("I will message you to continue.")
+        await
+        self.contact_for_parrot(game, author, server)
 
     async def contact_for_parrot(self, game: str, author, server=None):
 
-            dm = await self.bot.send_message(author,
-                                             "Please respond to this message"
-                                             "with the details of your raid.\r\n If "
-                                             "you do not want a details, wait 30s\r\n"
-                                             "Ex: `Prestige Argos` ; `WoW Mythics` ; `LFR`")
-            desc = await self.bot.wait_for_message(channel=dm.channel,
-                                                    author=author, timeout=30)
+        dm = await
+        self.bot.send_message(author,
+                              "Please respond to this message"
+                              "with the details of your raid.\r\n If "
+                              "you do not want a details, wait 30s\r\n"
+                              "Ex: `Prestige Argos` ; `WoW Mythics` ; `LFR`")
 
-        if desc is None:
-            await self.bot.send_message(author,
-                                        "Okay, this one won't have a description.")
-        else:
-            dm = await self.bot.send_message(author,
-                                         "what time? Follow the example format. `MM/DD/YY Hr:MinAM/PM TIMEZONE` "
-                                         "Ex: `12/25/17 8:00pm PST`")
-        time = await self.bot.wait_for_message(channel=dm.channel,
-                                                  author=author, timeout=30)
-        if time is None:
-            return await self.bot.send_message(author,
-                                               "I can't wait forever, "
-                                               "try again when ready")
-        else:
-            await self.bot.say(game, desc, time, server)
-            await self.bot.send_message(author, "Your raid was created")
+        desc = await
+        self.bot.wait_for_message(channel=dm.channel,
+                                  author=author, timeout=30)
+
+    if desc is None:
+
+        await
+        self.bot.send_message(author,
+                              "Okay, this one won't have a description.")
+    else:
+        dm = await
+        self.bot.send_message(author,
+                              "what time? Follow the example format. `MM/DD/YY Hr:MinAM/PM TIMEZONE` "
+                              "Ex: `12/25/17 8:00pm PST`")
+    time = await
+    self.bot.wait_for_message(channel=dm.channel,
+                              author=author, timeout=30)
+    if time is None:
+        return await
+        self.bot.send_message(author,
+                              "I can't wait forever, "
+                              "try again when ready")
+    else:
+        await
+        self.bot.say(game, desc, time, server)
+        await
+        self.bot.send_message(author, "Your raid was created")
+
 
 def setup(bot):
     bot.add_cog(parrot(bot))
