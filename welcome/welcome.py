7db4435543e5389158097d57d5c3ee0eef9c17e3
therealshibe <andrewdkennedy1@gmail.com>
2018-05-01 22:26:49 -0700

    asdf
    asdf

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -13,7 +13,7 @@ class welcome:
     async def welcome(self, ctx, user_id: str):
         """welcome a new DOGE using ShibBot with hardcoded welcome message in a DM"""
         client = discord.Client()
-        user =  client.users.get("name", user_id).id
+        user =  client.users.get.user_id.id
         msg = '*Hello, this is doge!* :wolf:\r\nI\'m here to inform you of the new role you\'ve been granted by the mighty doges of the Thunderdoge.\r\nMuch congrats!\r\nNow that you\'re officially part of the pack you can use bot commands in `#botplayground` to assign yourself roles and join game specific text channels.\r\nVisit the `#announcements` text channel under the \"General Text Chat\" category to view these roles\/commands.\r\nThank you for joining us!\r\nIf you need help with anything, please bork in the `#halp_pls` channel or bork at a DogeMaster\/DogeMod\r\nWelcome to the Thunderdoge!'
 
         try:
