b3ee2fffb5a35f0bf64ef2e2675afa17bf4f5b7b
therealshibe <andrewdkennedy1@gmail.com>
2018-05-01 21:34:34 -0700

    is fix
    asdf

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -4,19 +4,19 @@ from cogs.utils import checks
 from discord.ext import commands
 
 class welcome:
-    """PM People Using The Bot"""
+    """welcome a new DOGE Using The Bot with hardcoded welcome message"""
     def __init__(self, bot):
         self.bot = bot
-        msg = '*Hello, this is doge!* :wolf:\r\nI\'m here to inform you of the new role you\'ve been granted by the mighty doges of the Thunderdoge.\r\nMuch congrats!\r\nNow that you\'re officially part of the pack you can use bot commands in `#botplayground` to assign yourself roles and join game specific text channels.\r\nVisit the `#announcements` text channel under the \"General Text Chat\" category to view these roles\/commands.\r\nThank you for joining us!\r\nIf you need help with anything, please bork in the `#halp_pls` channel or bork at a DogeMaster\/DogeMod\r\nWelcome to the Thunderdoge!'
 
     @commands.command(pass_context=True)
     @checks.is_owner()
-    async def welcome(self, ctx, user_id: str):
-        """Sends a user the welcome message after a manual promote to DOGE"""
-        user = await self.bot.get_user_info(id)
+    async def welcome(self, ctx, user_id: str, *, msg='*Hello, this is doge!* :wolf:\r\nI\'m here to inform you of the new role you\'ve been granted by the mighty doges of the Thunderdoge.\r\nMuch congrats!\r\nNow that you\'re officially part of the pack you can use bot commands in `#botplayground` to assign yourself roles and join game specific text channels.\r\nVisit the `#announcements` text channel under the \"General Text Chat\" category to view these roles\/commands.\r\nThank you for joining us!\r\nIf you need help with anything, please bork in the `#halp_pls` channel or bork at a DogeMaster\/DogeMod\r\nWelcome to the Thunderdoge!'):
+        """welcome a new DOGE using ShibBot with hardcoded welcome message in a DM"""
+        user = await self.bot.get_user_info(user_id)
         try:
             e = discord.Embed(colour=discord.Colour.red())
-            e.title = "You've recieved a message from a DogeMod!"
+            e.title = "You've recieved a message from a developer!"
+            e.add_field(name="ShibeBot:", value="Bork", inline=False)
             e.add_field(name="Time:", value=datetime.datetime.now().strftime("%A, %B %-d %Y at %-I:%M%p").replace("PM", "pm").replace("AM", "am"), inline=False)
             e.add_field(name="Message:", value=msg, inline=False)
             e.set_thumbnail(url=ctx.message.author.avatar_url)
