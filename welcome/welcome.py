710d0822ccc743f55ed490309a1438315f6f7486
therealshibe <andrewdkennedy1@gmail.com>
2018-05-01 21:47:37 -0700

    Convert
    name to id

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -12,14 +12,14 @@ class welcome:
     @checks.is_owner()
     async def welcome(self, ctx, user_id: str, msg='*Hello, this is doge!* :wolf:\r\nI\'m here to inform you of the new role you\'ve been granted by the mighty doges of the Thunderdoge.\r\nMuch congrats!\r\nNow that you\'re officially part of the pack you can use bot commands in `#botplayground` to assign yourself roles and join game specific text channels.\r\nVisit the `#announcements` text channel under the \"General Text Chat\" category to view these roles\/commands.\r\nThank you for joining us!\r\nIf you need help with anything, please bork in the `#halp_pls` channel or bork at a DogeMaster\/DogeMod\r\nWelcome to the Thunderdoge!'):
         """welcome a new DOGE using ShibBot with hardcoded welcome message in a DM"""
-        user = await self.bot.get_user_info(user_id)
+        username = await self.bot.get_member(user_id)
+        user = await self.bot.get_user_info(username)
         try:
             e = discord.Embed(colour=discord.Colour.red())
-            e.title = "You've recieved a message from a developer!"
-            e.add_field(name="ShibeBot:", value="Bork", inline=False)
+            e.title = "You've recieved a message from a DogeMod!"
             e.add_field(name="Time:", value=datetime.datetime.now().strftime("%A, %B %-d %Y at %-I:%M%p").replace("PM", "pm").replace("AM", "am"), inline=False)
             e.add_field(name="Message:", value=msg, inline=False)
-            e.set_thumbnail(url=ctx.message.author.avatar_url)
+            e.set_thumbnail(url="https://cdn.discordapp.com/avatars/275738057200631819/0a79e4457eb45374f7bcc9d5b8a981b3.png")
             await self.bot.send_message(user, embed=e)
         except:
             await self.bot.say(':x: Failed to send message to user_id `{}`.'.format(user_id))
