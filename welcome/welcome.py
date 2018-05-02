481ebe44421dfde1cdae9d52bf19e1cb706aa1cb
therealshibe <andrewdkennedy1@gmail.com>
2018-05-01 22:35:13 -0700

    Spelling
    lol "shibBot"

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -4,14 +4,14 @@ from cogs.utils import checks
 from discord.ext import commands
 
 class welcome:
-    """welcome a new DOGE Using The Bot with hardcoded welcome message"""
+    """welcome a new DOGE Using ShibeBot with hardcoded welcome message"""
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(pass_context=True)
     @checks.is_owner()
     async def welcome(self, ctx, user: discord.Member):
-        """welcome a new DOGE using ShibBot with hardcoded welcome message in a DM"""
+        """welcome a new DOGE using ShibeBot with hardcoded welcome message in a DM"""
         msg = '*Hello, this is doge!* :wolf:\r\nI\'m here to inform you of the new role you\'ve been granted by the mighty doges of the Thunderdoge.\r\nMuch congrats!\r\nNow that you\'re officially part of the pack you can use bot commands in `#botplayground` to assign yourself roles and join game specific text channels.\r\nVisit the `#announcements` text channel under the \"General Text Chat\" category to view these roles\/commands.\r\nThank you for joining us!\r\nIf you need help with anything, please bork in the `#halp_pls` channel or bork at a DogeMaster\/DogeMod\r\nWelcome to the Thunderdoge!'
 
         try:
