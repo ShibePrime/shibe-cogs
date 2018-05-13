54b4e48dd1882e232c5e0db3a627f6240b5db419
therealshibe <andrewdkennedy1@gmail.com>
2018-05-13 14:56:52 -0700

    update
    update

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -12,7 +12,7 @@ class welcome:
     @checks.is_owner()
     async def welcome(self, ctx, user: discord.Member):
         """welcome a new DOGE using ShibeBot with hardcoded welcome message in a DM"""
-        msg = '*Hello, this is doge!* :wolf:\r\nI\'m here to inform you of the new role you\'ve been granted by the mighty doges of the Thunderdoge.\r\nMuch congrats!\r\nNow that you\'re officially part of the pack you can use bot commands in `#botplayground` to assign yourself roles and join game specific text channels.\r\nVisit the `#announcements` text channel under the \"General Text Chat\" category to view these roles\/commands.\r\nThank you for joining us!\r\nIf you need help with anything, please bork in the `#halp_pls` channel or bork at a DogeMaster\/DogeMod\r\nWelcome to the Thunderdoge!'
+        msg = '*Hello, this is doge!* :wolf:\r\nI\'m here to inform you of the new role you\'ve been granted by the mighty doges of the Thunderdoge.\r\nMuch congrats!\r\nNow that you\'re officially part of the pack you can use bot commands in `#botplayground` to assign yourself roles and join game specific text channels.\r\nVisit the `#selfroles` text channel under the \"General Text Chat\" category to view these roles\/commands.\r\nThank you for joining us!\r\nIf you need help with anything, please bork in the `#halp_pls` channel or bork at a DogeMaster\/DogeMod\r\nWelcome to the Thunderdoge!'
 
         try:
             e = discord.Embed(colour=discord.Colour.red())
