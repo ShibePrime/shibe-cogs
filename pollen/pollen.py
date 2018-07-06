4fc68336e9c8f3028311b54914bc434e8d3dbe51
therealshibe <andrewdkennedy1@gmail.com>
2018-07-06 16:05:16 -0700

    asdfasdf
    asdfasdf

--- pollen/pollen.py
+++ pollen/pollen.py
@@ -18,8 +18,8 @@ class pollen:
             await self.run(websession)
 
     async def run(self, websession):
-            client = await pypollencom.Client(98908, websession)
-            data = await client.allergens.current()
+            client = pypollencom.Client(98908, websession)
+            data = client.allergens.current()
 #            em = discord.Embed(title='', description=data, colour=0x6FA8DC, )
 #            await self.bot.say(embed=em)
             await self.bot.say(data)
