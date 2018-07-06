2b38bd2224ba3493f89e2b0485ef235ba75253f2
therealshibe <andrewdkennedy1@gmail.com>
2018-07-06 14:40:23 -0700

    simple
    geometry

--- pollen/pollen.py
+++ pollen/pollen.py
@@ -1,72 +1,28 @@
+import json
 import discord
+from aiohttp import ClientSession
 from discord.ext import commands
-from .utils.chat_formatting import pagify
-import aiohttp
 
-try:  # check if BeautifulSoup4 is installed
-    from bs4 import BeautifulSoup
-    soup_available = True
-except:
-    soup_available = False
-
-try:  # check if pypandoc is installed
-    import pypandoc
-    pypandoc_available = True
-except:
-    pypandoc_available = False
-
-base_url = 'https://www.pollen.com/api/forecast/current/pollen/98908'
-headers = {'Content-Type': 'application/json; charset=utf-8','Referer': 'https://www.pollen.com/forecast/current/pollen/98908', 'Accept': 'application/json, text/plain, */*'}
+url = 'https://www.pollen.com/api/forecast/current/pollen/98908'
+headers = {'Content-Type': 'application/json; charset=utf-8','Referer': 'https://www.pollen.com/forecast/current/pollen/98908'}
 
 
 class pollen:
-
-    """Much Pollen , very area , wow """
+    """get pollen"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="pollen", pass_context=True)
     async def pollen(self, ctx):
-        """how many pollen? """
+        await self.get_pollen()
+        """Prints pollen zip"""
 
-        url = ''.join([base_url])
-
-        await self.print_pollen(url)
-
-    async def print_pollen(self, url):
-        try:
-            async with aiohttp.get(url, headers=headers) as response:
-                soup = BeautifulSoup(await response.text())
-
-#            pollendata = soup.find('div', {"class": "row text-center"})
-#            text_notes = pypandoc.convert_text(html_notes, 'plain',
-#                                               format='html',
-#                                               extra_args=['--wrap=none'])
-#            text_notes = text_notes.replace('&nbsp;', ' ')
-#            text_notes = text_notes.replace('&apos;', "'")
-#this is the only way I know howto cleanup the formatting. pls halp @TheRealShibe
-#            text_notes = text_notes.replace('+', "")
-#            text_notes = text_notes.replace('-', "")
-#            text_notes = text_notes.replace('-', "")
-#            text_notes = text_notes.replace('|', '')
-#            text_notes = text_notes.replace("Buy Price", "Buy Price \n")
-#            text_notes = text_notes.replace("24Hour Range", "24Hour Range \n")
-#            text_notes = text_notes.replace("API Result", "API Result \n")
-#            text_notes = text_notes.replace("Updated", "Updated \n")
-            em = discord.Embed(title='Pollen', description=soup, colour=0xFFD966)
-            await self.bot.say(embed=em)
-
-        except:
-            await self.bot.say("Error")
-
+    async def get_pollen(self):
+        async with ClientSession() as session:
+            async with session.get(url, headers=headers) as response:
+                em = discord.Embed(title='', description=pollen, colour=0x6FA8DC, )
+                await self.bot.say(embed=em)
 
 def setup(bot):
-    if soup_available and pypandoc_available:
-        bot.add_cog(pollen(bot))
-    else:
-        if not soup_available:
-            error_text += "You need to run `pip install beautifulsoup4`\n"
-        if not pypandoc_available:
-            error_text += "You need to run `pip install pypandoc`\n"
-        raise RuntimeError(error_text)
+    bot.add_cog(pollen(bot))
