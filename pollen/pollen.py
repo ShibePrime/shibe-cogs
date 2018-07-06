128336882d02640be8f835d0c487053dbb95ba07
therealshibe <andrewdkennedy1@gmail.com>
2018-07-06 16:44:37 -0700

    ID
    SDF

--- pollen/pollen.py
+++ pollen/pollen.py
@@ -1,9 +1,11 @@
+import json
 import discord
-import pypollencom
-import asyncio
 from aiohttp import ClientSession
 from discord.ext import commands
-from pypollencom import Client
+
+url = 'https://www.pollen.com/api/forecast/current/pollen/98908'
+headers = {'Content-Type': 'application/json; charset=utf-8','Referer': 'https://www.pollen.com/forecast/current/pollen/98908'}
+
 
 class pollen:
     """get pollen"""
@@ -12,26 +14,16 @@ class pollen:
         self.bot = bot
 
     @commands.command(name="pollen", pass_context=True)
-    async def main(self) -> None:
-        """Create the aiohttp session and run the example."""
-        async with ClientSession() as websession:
-            await run(websession)
+    async def pollen(self, ctx):
+        await self.get_pollen()
+        """Prints pollen"""
 
-    async def run(self, websession):
-            client = pypollencom.Client(98908, websession)
-            data = client.allergens.current()
-            em = discord.Embed(title='', description=data, colour=0x6FA8DC, )
-            await self.bot.say(embed=em)
-
-
-<<<<<<< HEAD
-=======
     async def get_pollen(self):
         async with ClientSession() as session:
             async with session.get(url, headers=headers) as response:
-                em = discord.Embed(title='', description=pollen, colour=0x6FA8DC, )
-                await self.bot.say(embed=em)
-
->>>>>>> parent of 4f1202c... oh
+                pollen = await response.json()
+#                em = discord.Embed(title='', description=pollenstr, colour=0x6FA8DC, )
+                await self.bot.say(pollen)
+                return pollen
 def setup(bot):
     bot.add_cog(pollen(bot))
