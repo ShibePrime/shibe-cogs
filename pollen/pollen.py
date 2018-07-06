8615d16dcbbfb6e10eb5f6b5b0a51870ebe80305
therealshibe <andrewdkennedy1@gmail.com>
2018-07-06 15:37:59 -0700

    a
    a

--- pollen/pollen.py
+++ pollen/pollen.py
@@ -13,16 +13,8 @@ class pollen:
 
     @commands.command(name="pollen", pass_context=True)
     async def pollen(self, ctx):
-        await self.get_pollen()
-        """Prints pollen"""
-
-    async def get_pollen(self):
-        async with ClientSession() as websession:
-            await self.run(websession)
-
-    async def run(websession):
         client = pypollencom.Client(98908, websession)
-        data = client.allergens.current()
+        data = Client.allergens.current()
         em = discord.Embed(title='', description=data, colour=0x6FA8DC, )
         await self.bot.say(embed=em)
 
