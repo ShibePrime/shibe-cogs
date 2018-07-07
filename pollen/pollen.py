67803fadd5781bb235b1041170c6c1190c0e962a
therealshibe <andrewdkennedy1@gmail.com>
2018-07-06 18:30:20 -0700

    copy
    that

--- pollen/pollen.py
+++ pollen/pollen.py
@@ -23,9 +23,12 @@ class pollen:
             async with session.get(url + str(zip), headers=headers) as response:
                 pollen = await response.json()
 #                em = discord.Embed(title='', description=pollenstr, colour=0x6FA8DC, )
-                location = pollen["Location"]
-                area = location["ZIP"]["City"]["State"]
-                await self.bot.say(area)
+                zip = pollen["Location"]["ZIP"]
+                city = pollen["Location"]["City"]
+                state = pollen["Location"]["WA"]
+                await self.bot.say(zip)
+                await self.bot.say(city)
+                await self.bot.say(state)
 
 def setup(bot):
     bot.add_cog(pollen(bot))
